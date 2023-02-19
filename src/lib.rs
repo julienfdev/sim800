132ecc6d205ae46d8132296c62cc07aae47b11df
Julien FERAND <hello@julienferand.dev>
2023-02-19 20:30:50 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! # SIM800
+//!
+//! `sim800` is a crate managing the communication with SIMCom SIM800 L/H modules. It handles
+//! connection to a serial port on which a sim module receiving AT commands is connected.
+//! a new instance can be configured passing a Sim800Config
+//!
+//! ## Roadmap
+//!
+//! The project will begin active development during Q2 2023, frequent updates will be provided
